4b6d00f93cd68773ff1f03ddb10df16e06b929fe
Alexey Prokhorenko <alexey1214@gmail.com>
2021-03-13 21:46:21 +0300

    Install psycopg, set PostgreSQL as default

--- candy_shop/candy_shop/settings/base.py
+++ candy_shop/candy_shop/settings/base.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'candy_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'candy_shop',
+        'USER': 'candy_shop',
+        'PASSWORD': 'candy_shop',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
